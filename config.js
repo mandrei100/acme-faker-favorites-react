9e8f878d05785251d5031879131e71aec4a28820
mandrei100 <mcrisandreica@hotmail.com>
2019-10-26 13:40:52 -0400

    rendered react elements

--- config.js
+++ config.js
@@ -1,9 +1,38 @@
 console.log(React);
 console.log(ReactDOM);
-let randomName = faker.name.findName();  // name
-let randomEmail = faker.internet.email(); // email
-
-console.log(randomCard);
-console.log(randomName);
 
 
+let userArray = [];
+
+function loopThrough() {
+    let arr = [];
+    let count = 10;
+    for (var i = 0; i < count; i++){
+        userArray.push({'name': faker.name.findName(), 'email': faker.internet.email()});
+    }
+    return arr;
+}
+loopThrough();
+// console.log(userArray);
+
+class Favorites extends React.Component {
+    render() {
+        console.log(userArray);
+        return (
+            <div>
+            <ul>
+                {userArray.map((element, idx) => {
+                    return <li id={idx} key={idx}>{element.name}<br></br>{element.email}</li>
+                })}
+            </ul>
+            </div>
+        )
+    }
+}
+
+
+
+ReactDOM.render(
+    <Favorites name="ourList" />,
+    document.getElementById('root')
+  );
